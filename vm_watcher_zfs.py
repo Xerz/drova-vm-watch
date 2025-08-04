20780f0d06357c5ccf2fd5298a559a02255e8fbd
Xerz <alehag@ya.ru>
2025-08-05 01:17:05 +0500

    zfs version wip

--- vm_watcher_zfs.py
+++ vm_watcher_zfs.py
@@ -2,6 +2,7 @@
 import subprocess
 import time
 import libvirt
+import requests
 import json
 import logging
 import os
@@ -9,6 +10,10 @@ from dotenv import load_dotenv
 
 # Загружаем переменные из .env
 load_dotenv()
+
+SERVER_UUID = os.getenv("SERVER_UUID")
+USER_ID = os.getenv("USER_ID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 VM_NAME = os.getenv("VM_NAME", "my-vm")
 SLEEP_TIME = int(os.getenv("SLEEP_TIME", "10"))
@@ -20,6 +25,20 @@ ZFS_SNAPSHOTS = [
     f"tank/{VM_NAME}@clean",
     f"tank/{VM_NAME}-storage@clean"
 ]
+
+
+ENDPOINT = f"https://services.drova.io/server-manager/servers/{SERVER_UUID}?user_id={USER_ID}"
+HEADERS = {"X-Auth-Token": AUTH_TOKEN}
+
+def get_state():
+    try:
+        r = requests.get(ENDPOINT, headers=HEADERS, timeout=5)
+        r.raise_for_status()
+        data = r.json()
+        return data.get("state")
+    except Exception as e:
+        logger.error(f"Ошибка запроса: {e}")
+        return None
 
 # Настройка логгера
 logging.basicConfig(
@@ -101,19 +120,39 @@ def main():
 
     in_session = False
 
+
     while True:
-        running = is_process_running(dom, PROCESS_NAME)
+        # Ждем BUSY или HANDSHAKE
+        waiting_msg_printed = False
+        while True:
+            state = get_state()
+            if state in ("BUSY", "HANDSHAKE"):
+                logger.info(f"VM {VM_NAME} entered session state: {state}")
+                break
+            if not waiting_msg_printed:
+                logger.info(f"Waiting for BUSY/HANDSHAKE (current: {state})")
+                waiting_msg_printed = True
+            else:
+                logger.debug(f"Still waiting (state={state})")
+            time.sleep(SLEEP_TIME)
 
-        if not in_session and running:
-            in_session = True
-            logger.info(f"Процесс {PROCESS_NAME} запущен → сессия началась")
-
-        elif in_session and not running:
-            in_session = False
-            logger.info(f"Процесс {PROCESS_NAME} завершён → выполняем восстановление ВМ")
-            reset_vm(dom)
-
-        time.sleep(SLEEP_TIME)
+        # Ждем LISTEN
+        waiting_msg_printed = False
+        while True:
+            state = get_state()
+            if state == "LISTEN":
+                logger.info(f"State changed to LISTEN → rebooting {VM_NAME}")
+                try:
+                    reset_vm(dom)
+                except libvirt.libvirtError as e:
+                    logger.error(f"Ошибка при перезагрузке: {e}")
+                break
+            if not waiting_msg_printed:
+                logger.info(f"Waiting for LISTEN (current: {state})")
+                waiting_msg_printed = True
+            else:
+                logger.debug(f"Still not LISTEN (state={state})")
+            time.sleep(SLEEP_TIME)
 
 
 if __name__ == "__main__":
