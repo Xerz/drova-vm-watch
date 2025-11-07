38d7bdfe931196a330a17a42dfb21ece6d489e44
Xerz <alehag@ya.ru>
2025-11-07 22:42:35 +0500

    переход на токен из QR-кода и на

--- vm_watcher_zfs.py
+++ vm_watcher_zfs.py
@@ -9,33 +9,6 @@ import os
 from dotenv import load_dotenv
 import obsws_python as obs
 
-# Загружаем переменные из .env
-load_dotenv()
-
-SERVER_UUID = os.getenv("SERVER_UUID")
-USER_ID = os.getenv("USER_ID")
-AUTH_TOKEN = os.getenv("AUTH_TOKEN")
-
-VM_NAME = os.getenv("VM_NAME", "my-vm")
-SLEEP_TIME = int(os.getenv("SLEEP_TIME", "10"))
-PROCESS_NAME = os.getenv("PROCESS_NAME", "ese.exe")
-SNAPSHOT_NAME = os.getenv("SNAPSHOT_NAME", "clean")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-ZFS_SNAPSHOTS = [
-    f"tank/{VM_NAME}@clean",
-    f"tank/{VM_NAME}-storage@clean"
-]
-GRACE_PERIOD = int(os.getenv("GRACE_PERIOD", "60")) # время, которое даётся ВМ для мягкого выключения
-CHECK_INTERVAL = 2  # секунды между проверками статуса ВМ при выключении
-
-OBS_WS_HOST = os.getenv("OBS_WS_HOST", "localhost")
-OBS_WS_PORT = int(os.getenv("OBS_WS_PORT", "4444"))
-OBS_WS_PASSWORD = os.getenv("OBS_WS_PASSWORD", "")
-
-ENDPOINT = "https://services.drova.io/session-manager/sessions"
-HEADERS = {"X-Auth-Token": AUTH_TOKEN}
-
-ACTIVE_SESSION_STATUSES = ("ACTIVE", "HANDSHAKE", "NEW")  # Статусы активной сессии
 
 # Настройка логгера
 logging.basicConfig(
@@ -44,10 +17,92 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def get_state():
+# Загружаем переменные из .env
+load_dotenv()
+
+# AUTH_TOKEN берём из token.json
+with open("token.json", "r") as f:
+    token_data = json.load(f)
+AUTH_TOKEN = token_data.get("auth_token")
+
+VM_NAME = os.getenv("VM_NAME", "my-vm")
+SLEEP_TIME = int(os.getenv("SLEEP_TIME", "10"))
+SNAPSHOT_NAME = os.getenv("SNAPSHOT_NAME", "clean")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+ZFS_SNAPSHOTS = [
+    f"tank/{VM_NAME}@clean",
+    f"tank/{VM_NAME}-storage@clean"
+]
+GRACE_PERIOD = int(os.getenv("GRACE_PERIOD", "120")) # время, которое даётся ВМ для мягкого выключения и станции для перехода из занятого состояния
+CHECK_INTERVAL = 1  # секунды между проверками статуса ВМ при выключении
+
+OBS_WS_HOST = os.getenv("OBS_WS_HOST", "localhost")
+OBS_WS_PORT = int(os.getenv("OBS_WS_PORT", "4444"))
+OBS_WS_PASSWORD = os.getenv("OBS_WS_PASSWORD", "")
+
+SERVER_UUID = os.getenv("SERVER_UUID")
+USER_ID = os.getenv("USER_ID")
+SESSIONS_ENDPOINT = "https://services.drova.io/session-manager/sessions"
+SERVER_ENDPOINT = f"https://services.drova.io/server-manager/servers/{SERVER_UUID}"
+
+# пустой POST https://services.drova.io/server-manager/servers/{SERVER_UUID}/set_published/true — убрать сервер в приват
+# пустой POST https://services.drova.io/server-manager/servers/{SERVER_UUID}/set_published/false — опубликовать сервер
+VISIBILITY_ENDPOINT = f"https://services.drova.io/server-manager/servers/{SERVER_UUID}/set_published/"
+
+# POST https://services.drova.io/token-verifier/renewProxyToken with body {"proxy_token": AUTH_TOKEN}, возвращает
+# {
+# 	"proxyToken": "newToken",
+# 	"verificationStatus": "success",
+# 	"client_id": USER_ID,
+# 	"client_roles": [
+# 		"client",
+# 		"merchant"
+# 	],
+# 	"server_id": null,
+# 	"session_id": null
+# }
+RENEWAL_ENDPOINT = f"https://services.drova.io/server-manager/servers/{SERVER_UUID}/renew"
+HEADERS = {"X-Auth-Token": AUTH_TOKEN}
+
+
+def request_token_renewal():
+    try:
+        r = requests.post(RENEWAL_ENDPOINT, json={"proxy_token": AUTH_TOKEN}, timeout=5)
+        r.raise_for_status()
+        data = r.json()
+        new_token = data.get("proxyToken")
+        if new_token:
+            global AUTH_TOKEN
+            AUTH_TOKEN = new_token
+            HEADERS["X-Auth-Token"] = AUTH_TOKEN
+            logger.info("Token renewed successfully")
+        else:
+            logger.error("Failed to renew token: no new token in response")
+    except Exception as e:
+        logger.error(f"Ошибка при продлении токена: {e}")
+
+# Функция для request и при необходимости обновления токена один раз перед окончательной ошибкой
+def safe_request(method, url, **kwargs):
+    try:
+        r = requests.request(method, url, headers=HEADERS, timeout=5, **kwargs)
+        r.raise_for_status()
+        return r
+    except requests.HTTPError as e:
+        if e.response.status_code == 401:  # Unauthorized, возможно токен истёк
+            logger.info("Token expired, attempting to renew")
+            request_token_renewal()
+            r = requests.request(method, url, headers=HEADERS, timeout=5, **kwargs)
+            r.raise_for_status()
+            return r
+        else:
+            raise
+
+def get_last_session_status():
     try:
         params = [("status", status) for status in ACTIVE_SESSION_STATUSES]
-        r = requests.get(ENDPOINT, params=params, headers=HEADERS, timeout=5)
+        params.append(("server_id", SERVER_UUID))
+        params.append(("limit" , "2"))
+        r = safe_request("GET", SESSIONS_ENDPOINT, params=params)
         r.raise_for_status()
         sessions = r.json().get("sessions", [])
         if not sessions:
@@ -57,12 +112,32 @@ def get_state():
         data = sessions[0]
         server_id = data.get("server_id")
         if server_id != SERVER_UUID:
+            # Скорее всего, теперь бесполезная проверка (мы передаём параметр server_id)
             raise RuntimeError("Server UUID does not match, check your Token-Server pair")
 
         return data.get("status")
     except Exception as e:
         logger.error(f"Ошибка запроса: {e}")
         return None
+
+def get_station_status():
+    try:
+        r = safe_request("GET", SERVER_ENDPOINT)
+        r.raise_for_status()
+        data = r.json()
+        return data.get("state")
+    except Exception as e:
+        logger.error(f"Ошибка запроса статуса сервера: {e}")
+        return None
+
+def set_station_published(published: bool):
+    try:
+        url = VISIBILITY_ENDPOINT + ("true" if published else "false")
+        r = safe_request("POST", url)
+        r.raise_for_status()
+        logger.info(f"Set server published={published} successfully")
+    except Exception as e:
+        logger.error(f"Ошибка установки видимости сервера: {e}")
 
 def reset_vm(dom):
     try:
@@ -129,6 +204,9 @@ def stop_record_and_wait(client: obs.ReqClient):
         time.sleep(0.5)
     return out_path
 
+ACTIVE_SESSION_STATUSES = ("ACTIVE", "HANDSHAKE", "NEW")  # Статусы активной сессии
+BUSY_STATION_STATUSES = ("BUSY", "HANDSHAKE")
+
 def main():
     conn = libvirt.open("qemu:///system")
     if conn is None:
@@ -148,7 +226,7 @@ def main():
         # Ждем любой из статусов активной сессии
         waiting_msg_printed = False
         while True:
-            state = get_state()
+            state = get_last_session_status()
             if state in ACTIVE_SESSION_STATUSES:
                 logger.info(f"VM {VM_NAME} entered session state: {state}")
                 try:
@@ -171,13 +249,30 @@ def main():
         # Ждем, когда статус станет неактивным
         waiting_msg_printed = False
         while True:
-            state = get_state()
+            state = get_last_session_status()
             if state and state not in ACTIVE_SESSION_STATUSES:
-                logger.info(f"State changed to {state} → reverting {VM_NAME}")
+                logger.info(f"Session state changed to {state}")
+
+                # Переводим сервера в приватный режим
+                set_station_published(published=False)
+
+                logger.info(f"Waiting for server station to be not in {BUSY_STATION_STATUSES} (current: {station_status})")
+
+                # Ждем, пока сервер станет не BUSY или не пройдёт ограничение по времени
+                waited = 0
+                while True:
+                    station_status = get_station_status()
+                    if station_status not in BUSY_STATION_STATUSES and waited < GRACE_PERIOD:
+                        logger.info(f"Server station status is now {station_status} → proceeding to reset VM {VM_NAME}")
+                        break
+                    time.sleep(SLEEP_TIME)
+                    waited += SLEEP_TIME
+
                 try:
                     reset_vm(dom)
                 except libvirt.libvirtError as e:
                     logger.error(f"Ошибка при перезагрузке: {e}")
+
                 try:
                     obs_client = obs.ReqClient(host=OBS_WS_HOST, port=OBS_WS_PORT, password=OBS_WS_PASSWORD)
                     obs_client.get_version()
@@ -186,6 +281,8 @@ def main():
                     logger.error(f"Ошибка при остановке записи в OBS: {e}")
                 finally:
                     obs_client = None
+
+                set_station_published(published=True)
                 break
             if not waiting_msg_printed:
                 logger.info(f"Waiting for not in {ACTIVE_SESSION_STATUSES} (current: {state})")
